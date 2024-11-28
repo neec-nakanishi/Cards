edd5797f3fda2ba4f1f5ae18b514c19cf1f933b1
neec-nakanishi <nakanishi@g.neec.ac.jp>
2024-11-28 15:18:34 +0900

    シャッフル機能を追加

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         Card[] cards = new Card[52];
@@ -9,8 +11,21 @@ public class Main {
             }
         }
 
+        Main.shuffle(cards);
+
         for (int i=0; i<cards.length;i++) {
             System.out.println(cards[i].mark + cards[i].number);
+        }
+    }
+
+    static void shuffle(Card[] cards) {
+        for(int i=0 ; i<1000 ; i++) {
+            Random rnd = new Random();
+            int r1 = rnd.nextInt(cards.length);
+            int r2 = rnd.nextInt(cards.length);
+            Card tmp = cards[r1];
+            cards[r1] = cards[r2];
+            cards[r2] = tmp;
         }
     }
 }
